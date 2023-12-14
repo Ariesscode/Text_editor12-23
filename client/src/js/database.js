05f4fa0591e5b4630633dc2558f9100055483ddb
Abigail Burtley <abigailtop95@yahoo.com>
2023-12-14 02:36:46 -0500

    unfinished assignment

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,24 +15,56 @@ const initdb = async () =>
 //content is added to the object store using add method
 //wait for completion 
 //console log results added to db
-  export const putDb = async (content) => {
-    const db = await initdb();
-    const tx = db.transaction('jate', 'readwrite');
-    const store = tx.objectStore('jate');
-    const result = await store.add(content);
-    await tx.done;
-    console.log('Contents added:', result);
-  };
-  
-  // Add logic for a method that gets all the content from the database/
-// pulling from db store , we are pulling jate from the object store 
-  export const getDb = async () => {
-    const db = await initdb();
-    const tx = db.transaction('jate', 'readonly');
-    const store = tx.objectStore('jate');
-    const result = await store.getAll();
-    await tx.done;
-    console.log('Retrieved contents:', result);
-    return result;
-  };
+export const postDb = async (content) => {
+  console.log('Post to the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.add({ todo: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+export const getOneDb = async (id) => {
+  console.log('GET from the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+export const deleteDb = async (id) => {
+  console.log('DELETE from the database', id);
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.delete(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: id, jate: content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
 initdb();
--- client/src/js/header.js
+++ client/src/js/header.js
@@ -1,14 +1,12 @@
 const header = `
-/*
+
        _____  ____________
       / /   |/_  __/ ____/
  __  / / /| | / / / __/   
 / /_/ / ___ |/ / / /___   
  ____/_/  |_/_/ /_____/   
 just another text editor
-*/                          
+                         
 `;
-const headerContainer = document.getElementById('header-container');
 
-headerContainer.textContent = header;
 export { header };
